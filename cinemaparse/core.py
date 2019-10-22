0948aa0a94b56e7618bbc5e603a430cc55385a5c
MrBenz <dkostya05@gmail.com>
2019-10-22 18:57:13 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,9 +1,15 @@
 class CinemaParser(object):
+    
     def __init__(self):
         spb_parser = CinemaParser('spb')
         msk_parser = CinemaParser()
         pass
+    
+    def extract_raw_content():
+        pass
 
+    def print_raw_content():
+        pass
 
 
 
