e7abb0ba304a254233bdaa5917edf691f5c83f70
MrBenz <dkostya05@gmail.com>
2019-10-28 10:23:52 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,24 +1,31 @@
+# some_module.py
+"""Этот модуль содержит классы CinemaParser."""
 from bs4 import BeautifulSoup as bf
 import requests as req
+class CinemaParser:
+    """Класс, выполняющий действия над сайтами."""
+    def __init__(self, city):
+        """Город"""
+        self.town = city
+        self.content = None
+        self.soup = None
 
-class CinemaParser(object):
-    def __init__(self,object):
-            self.city = object
-
-    
-    def extract_raw_content(self,link):
-            self.content = req.get(link)
-            self.soup = bf(self.content.text, 'html')
+    def extract_raw_content(self, link):
+        """Метод берет код страници."""
+        self.content = req.get(link)
+        self.soup = bf(self.content.text, 'html')
 
 
     def print_raw_content(self):
-            right = self.soup.prettify()
-            return right
+        """Метод преобразовывает и выводит код страницы"""
+        right = self.soup.prettify()
+        return right
+
+    def get_films_list():
+        """Метод который возвращает список всех названий фильмов с главной страницы сайта."""
 
 
-
-
-msk_parser = CinemaParser('msk')
-link = "https://msk.subscity.ru/"
-msk_parser.extract_raw_content(link)
-print(msk_parser.print_raw_content())
+MSK_PARSER = CinemaParser('msk')
+LINK = "https://msk.subscity.ru/"
+MSK_PARSER.extract_raw_content(LINK)
+print(MSK_PARSER.print_raw_content())
