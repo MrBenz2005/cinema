ba78486cca0193fc2eeb3295281757d628198d85
MrBenz <dkostya05@gmail.com>
2019-11-05 20:24:48 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -13,19 +13,27 @@ class CinemaParser:
     def extract_raw_content(self, link):
         """Метод берет код страници."""
         self.content = req.get(link)
-        self.soup = bf(self.content.text, 'html')
+        self.soup = bf(self.content.text, 'html.parser')
 
 
     def print_raw_content(self):
         """Метод преобразовывает и выводит код страницы"""
-        right = self.soup.prettify()
-        return right
+        self.right = self.soup.prettify()
+        return self.right
 
-    def get_films_list():
+    def get_films_list(self,link):
+        film = []
+        self.content = req.get(link)
+        self.soup = bf(self.content.text, 'html.parser')
+        films = self.soup
+        films= films.find_all("div", class_='movie-plate')
+        for i in films:
+            film.append(i["attr-title"])
+        return film
         """Метод который возвращает список всех названий фильмов с главной страницы сайта."""
 
 
 MSK_PARSER = CinemaParser('msk')
 LINK = "https://msk.subscity.ru/"
 MSK_PARSER.extract_raw_content(LINK)
-print(MSK_PARSER.print_raw_content())
+print(MSK_PARSER.get_films_list(LINK))
