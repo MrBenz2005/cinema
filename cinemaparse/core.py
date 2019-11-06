3f2267fb0dbf8c927b1d26e67e6b362394571c6a
MrBenz <dkostya05@gmail.com>
2019-11-06 22:48:43 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -2,6 +2,8 @@
 """Этот модуль содержит классы CinemaParser."""
 from bs4 import BeautifulSoup as bf
 import requests as req
+
+
 class CinemaParser:
     """Класс, выполняющий действия над сайтами."""
     def __init__(self, city):
@@ -11,6 +13,17 @@ class CinemaParser:
         self.content = None
         self.soup = None
         self.LINK = "https://msk.subscity.ru/"
+
+    def minimum(self, list):
+        x = None
+        a = 1
+        for i in range(len(list)):
+            if i == 0:
+                x = list[i]
+            if list[i] < x:
+                x = list[i]
+                a = i
+        return a
 
     def extract_raw_content(self):
         """Метод берет код страници."""
@@ -49,13 +62,21 @@ class CinemaParser:
         film_page_link = film_link_a_tag["href"]
         film_page_response = req.get(self.LINK + film_page_link)
         film_page_response = bf(film_page_response.text, 'html.parser')
-        film1 = film_page_response.find_all("td", class_="text-center cell-screenings")
-        for item in film1
+        film1 = film_page_response.find_all("div", class_="cinema-name")
         film = film_page_response.find_all("td", class_="text-center cell-screenings")
-        """for j in film:
+        for j in film:
             self.spicok.append(j["attr-time"])
-        a = min(self.spicok)"""
-        return film
+        b = min(self.spicok)
+        print(b)
+        c = self.minimum(self.spicok)
+        print(c)
+        self.spicok.clear()
+        for item in film1:
+            item1 = item.text
+            self.spicok.append(item1)
+
+
+        return self.spicok
 
 
 MSK_PARSER = CinemaParser('msk')
