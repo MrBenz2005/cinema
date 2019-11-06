4e311f865e2445e2a333dd454959b9b381f1249c
MrBenz <dkostya05@gmail.com>
2019-11-06 19:52:40 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -6,6 +6,7 @@ class CinemaParser:
     """Класс, выполняющий действия над сайтами."""
     def __init__(self, city):
         """Город"""
+        self.spicok = []
         self.town = city
         self.content = None
         self.soup = None
@@ -23,28 +24,42 @@ class CinemaParser:
         return self.right
 
     def get_films_list(self):
-        film = []
+        self.films = []
         self.content = req.get(self.LINK)
         self.soup = bf(self.content.text, 'html.parser')
-        films = self.soup
-        films= films.find_all("div", class_='movie-plate')
-        for i in films:
-            film.append(i["attr-title"])
-        return film
+        film = self.soup
+        film= film.find_all("div", class_='movie-plate')
+        for i in film:
+            self.films.append(i["attr-title"])
+        return self.films
+
         """Метод который возвращает список всех названий фильмов с главной страницы сайта."""
 
-    def get_film_nearest_session(self):
+    def get_film_nearest_session(self,name):
         self.extract_raw_content()
+        self.get_films_list()
+        for i in range(len(self.films)):
+            if self.films[i] == name:
+                x = i
+                break
         link = self.soup
         all_films = link.find_all("div", class_='movie-plate')
-        first_film = all_films[0]
+        first_film = all_films[x]
         film_link_a_tag = first_film.find_all("a")[0]
         film_page_link = film_link_a_tag["href"]
         film_page_response = req.get(self.LINK + film_page_link)
-        film = film_page_response.find_all("td", class_='attr-time')
+        film_page_response = bf(film_page_response.text, 'html.parser')
+        film1 = film_page_response.find_all("td", class_="text-center cell-screenings")
+        for item in film1
+        film = film_page_response.find_all("td", class_="text-center cell-screenings")
+        """for j in film:
+            self.spicok.append(j["attr-time"])
+        a = min(self.spicok)"""
         return film
 
 
 MSK_PARSER = CinemaParser('msk')
-print(MSK_PARSER.get_film_nearest_session())
-
+a = 'Арахисовый сокол'
+b = 'Zомбилэнд: Контрольный выстрел'
+c = 'Бойцовский клуб'
+print(MSK_PARSER.get_film_nearest_session(b))
