0d1ee88b20338699ad92fa59a8cc8f6b5f50afb1
MrBenz <dkostya05@gmail.com>
2019-11-06 11:22:55 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -9,10 +9,11 @@ class CinemaParser:
         self.town = city
         self.content = None
         self.soup = None
+        self.LINK = "https://msk.subscity.ru/"
 
-    def extract_raw_content(self, link):
+    def extract_raw_content(self):
         """Метод берет код страници."""
-        self.content = req.get(link)
+        self.content = req.get(self.LINK)
         self.soup = bf(self.content.text, 'html.parser')
 
 
@@ -21,9 +22,9 @@ class CinemaParser:
         self.right = self.soup.prettify()
         return self.right
 
-    def get_films_list(self,link):
+    def get_films_list(self):
         film = []
-        self.content = req.get(link)
+        self.content = req.get(self.LINK)
         self.soup = bf(self.content.text, 'html.parser')
         films = self.soup
         films= films.find_all("div", class_='movie-plate')
@@ -32,8 +33,18 @@ class CinemaParser:
         return film
         """Метод который возвращает список всех названий фильмов с главной страницы сайта."""
 
+    def get_film_nearest_session(self):
+        self.extract_raw_content()
+        link = self.soup
+        all_films = link.find_all("div", class_='movie-plate')
+        first_film = all_films[0]
+        film_link_a_tag = first_film.find_all("a")[0]
+        film_page_link = film_link_a_tag["href"]
+        film_page_response = req.get(self.LINK + film_page_link)
+        film = film_page_response.find_all("td", class_='attr-time')
+        return film
+
 
 MSK_PARSER = CinemaParser('msk')
-LINK = "https://msk.subscity.ru/"
-MSK_PARSER.extract_raw_content(LINK)
-print(MSK_PARSER.get_films_list(LINK))
+print(MSK_PARSER.get_film_nearest_session())
+
