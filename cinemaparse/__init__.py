e795ae1b1d67d47c2e28f9de879aa40b8a0348ac
MrBenz <dkostya05@gmail.com>
2019-11-11 11:23:20 +0300

    Update __init__.py

--- cinemaparse/__init__.py
+++ cinemaparse/__init__.py
@@ -1,9 +1,5 @@
-import
-class CinemaParser(object):
-    def __init__(self):
-        spb_parser = CinemaParser('spb')
-        msk_parser = CinemaParser()
-        pass
+from .core import extract_raw_content
+
 
 
 
